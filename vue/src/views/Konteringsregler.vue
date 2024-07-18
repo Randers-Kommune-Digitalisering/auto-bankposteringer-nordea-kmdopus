3fea147c6bf765ca3668c283ed01923c9329296e
Søren Thomassen <115066094+st-randers@users.noreply.github.com>
2024-07-18 12:29:31 +0000

    Add simple rule search functionality

--- vue/src/views/Konteringsregler.vue
+++ vue/src/views/Konteringsregler.vue
@@ -3,13 +3,18 @@
     import Content from '@/components/Content.vue'
     import IconTable from '@/components/icons/IconTable.vue'
 
+    const allKonteringsregler = ref(null)
     const konteringsregler = ref(null)
+
+    const isSearching = ref(false)
+    const searchKeyword = ref("")
     
     // Fetch regler
     fetch('/api/konteringsregler')
         .then(response => response = response.json())
+        .then(value => allKonteringsregler.value = value)
         .then(value => konteringsregler.value = value)
-        .then(value => console.log(value))
+        //.then(value => console.log(value))
 
     const keyMap = {
         "id": {
@@ -50,6 +55,35 @@
     /* Example data format
         {"0":{"name":"Reference","value":"Rekvireret udb"},"1":{"name":"Advisliste"},"2":{"name":"Afsender"},"3":{"name":"Posteringstype","value":"Cap"},"4":{"name":"End-to-end-reference"},"5":{"name":"Beløb","operator":null},"6":{"Posteringstekst":"Tekst fra bank","Artskonto":"12340000","Notat":"Udbetaling"},"7":{"active":true},"8":{"ruleId":54},"9":{"exception":true}}
     */
+
+    function toggleSearch()
+    {
+        isSearching.value = !isSearching.value
+        if(!isSearching.value)
+            search(null)
+    }
+    function search(keyword)
+    {
+        console.log("Searching for " + keyword)
+        konteringsregler.value = searchList(allKonteringsregler.value, keyword)
+    }
+
+    function searchList(list, keyword)
+    {
+        keyword = keyword.toLowerCase()
+
+        console.log("Searching in list")
+        console.log(list)
+        
+        if(keyword == null)
+            return list
+        else 
+            return list.filter(x => ((x[keyMap.reference.id])[keyMap.reference.key] != null && x[keyMap.reference.id][keyMap.reference.key].toLowerCase().includes(keyword)) || /* Reference */
+                                    ((x[keyMap.afsender.id])[keyMap.afsender.key] != null && x[keyMap.afsender.id][keyMap.afsender.key].toLowerCase().includes(keyword)) || /* Afsender */
+                                    ((x[keyMap.notat.id])[keyMap.notat.key] != null && x[keyMap.notat.id][keyMap.notat.key].toLowerCase().includes(keyword)) || /* Notat */
+                                    (x[keyMap.ruleId.id])[keyMap.ruleId.key].value != null && x[keyMap.ruleId.id][keyMap.ruleId.key].toLowerCase().includes(keyword) || x.id == keyword ) /* RuleID / ID */
+    }
+
 </script>
 
 <template>
@@ -60,14 +94,26 @@
         <template #icon>
             <IconTable />
         </template>
-        <template #heading>Aktuelle konteringsregler</template>
-
+        <template #heading>
+            Aktuelle konteringsregler
+            
+            <div class="float-right searchButtonDiv">
+                <button :class="isSearching ? 'gray' : ''" @click="toggleSearch()">
+                    {{ isSearching ? 'Luk søgning' : 'Søg i regler'}}</button>
+            </div>
+        </template>
+        
         <span class="paragraph">
-            Herunder kan de aktuelle konteringsregler ses, rettes og slettes. Vær opmærksom på at rettelser overskrives hvis der laves ændringer i <code>konteringsregler.csv</code>.
+            Herunder kan de aktuelle konteringsregler ses, rettes og slettes. Vær opmærksom på at rettelser overskrives hvis der laves ændringer i <code>konteringsregler.json</code>.
         </span>
         
         <table>
             <thead>
+                <tr v-if="isSearching">
+                        <th :colspan="(Object.values(keyMap).filter(value => !value.hidden).length)+1">
+                            <input type="text" placeholder="Søg efter regel" v-model="searchKeyword" :onchange="search(searchKeyword)" />
+                        </th>
+                </tr>
                 <tr>
                     <th v-for="(value, key) in keyMap" :class="value.hidden ? 'hidden' : ''" class="capitalize">{{key}}</th>
                     <th></th>
@@ -76,7 +122,7 @@
             <tr v-for="(obj, index) in konteringsregler">
                 <td v-for="(value, key) in keyMap" :class="value.hidden ? 'hidden' : ''">{{ obj[value.id] != undefined ? (obj[value.id][value.key] ?? "") : obj[value.key] }}</td>
                 <td><router-link :to="'/retkonteringsregel/' + obj[keyMap['id'].key]">
-                        <button @click="">Redigér</button>
+                        <button class="editButton orange" @click="">Redigér</button>
                     </router-link></td>
             </tr>
         </table>
@@ -87,5 +133,17 @@
 <style scoped>
     .hidden {
         display:none;
+    }
+    td {
+        font-size: 0.8em;
+    }
+    .searchButtonDiv 
+    {
+        transform: translateY(-0.8rem);
+    }
+    .editButton
+    {
+        font-size: 0.9em;
+        max-width: 2rem;
     }
 </style>
