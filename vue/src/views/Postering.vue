8f145f96f2e73eb3e6a543b137998b6b8affa118
Christian Leonhardt <121159190+CSL-randers@users.noreply.github.com>
2025-05-15 13:42:03 +0200

    Update Postering.vue
    Signed-off-by: Christian Leonhardt <121159190+CSL-randers@users.noreply.github.com>

--- vue/src/views/Postering.vue
+++ vue/src/views/Postering.vue
@@ -100,13 +100,13 @@
     }
 
     function validateCPR(value) {
-        const regex = /((((0[1-9]|[12][0-9]|3[01])(0[13578]|10|12)(\d{2}))|(([0][1-9]|[12][0-9]|30)(0[469]|11)(\d{2}))|((0[1-9]|1[0-9]|2[0-8])(02)(\d{2}))|((29)(02)(00))|((29)(02)([2468][048]))|((29)(02)([13579][26])))[-]*\d{4})/gm;
+        const regex = /\b((0[1-9]|[12][0-9]|3[01])(0[13578]|10|12)|([0][1-9]|[12][0-9]|30)(0[469]|11)|(0[1-9]|1[0-9]|2[0-8])(02)|(29)(02)(00)|((29)(02)([2468][048]))|((29)(02)([13579][26])))(\d{2})(\d{4})\b/gm;
 
         if (!value) {
             // Allow empty CPR field
             errors.value.cpr = null;
         } else if (!regex.test(value)) {
-            errors.value.cpr = 'CPR skal matche formatet DDMMÅÅ-XXXX eller DDMMÅÅXXXX.';
+            errors.value.cpr = 'CPR skal matche formatet DDMMÅÅXXXX.';
         } else {
             errors.value.cpr = null;
         }
